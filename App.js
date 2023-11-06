594824804706dc2eb58cf58732b639e9904d884f
Archit <architojha420@gmail.com>
2023-11-06 06:16:46 +0530

    setting up tailwind

--- App.js
+++ App.js
@@ -3,9 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className={`flex-1 justify-center items-center `}>
+      <Text className={`text-black bg-white mt-100 `}>halalalalalal</Text>
     </View>
   );
 }
