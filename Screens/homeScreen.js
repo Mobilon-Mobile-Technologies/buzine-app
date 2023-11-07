489193f713427b79d86ec8ae674754a4754683ff
Archit <architojha420@gmail.com>
2023-11-07 16:10:47 +0530

    using axios to fetch data from json hosted on github

--- Screens/homeScreen.js
+++ Screens/homeScreen.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function HomeScreen () {
+export default function HomeScreen() {
+  const [data, setData] = useState([]);
+  const baseURL =
+  "https://raw.githubusercontent.com/Mobilon-Mobile-Technologies/buzine-app/archit-branch/Components/index.json?token=GHSAT0AAAAAACHSUEOT2WRUJRI35M5EW3XKZKKCP7Q";
+  useEffect(() => {
+    axios.get(baseURL)
+      .then(res => {
+        setData(res.data); // Update the state with the response data
+      })
+      .catch(err => {
+        console.log("Error: ", err);
+      });
+  }, []);
+
   return (
-    <View className={`flex-1 justify-center items-center`}>
-      <Text>HomeScreen</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Text>ID: {item.id}</Text>
+        )}
+        keyExtractor={(item) => item.id.toString()} // Provide a key extractor
+      />
     </View>
   );
 }
