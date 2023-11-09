2d6d24deb8b04e863fb768514d8007f8334261b3
Archit <architojha420@gmail.com>
2023-11-10 01:23:02 +0530

    fixed image loading

--- Components/postComponent.js
+++ Components/postComponent.js
@@ -1,17 +1,38 @@
-import { View, Text, Dimensions,Image } from 'react-native'
-import React from 'react'
+import { View, Text, Dimensions, Image } from "react-native";
+import React,{useState} from "react";
 
-const {width, height}=Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
-const cardWidth=width*0.45;
-const cardHeight=height*0.38
+const cardWidth = width * 0.45;
+const cardHeight = height * 0.38;
 
-export default function PostComponent({item}){
-  return (
-    <View style={{height: cardHeight, width: cardWidth, marginHorizontal: 20, marginVertical: 3, borderRadius: 20}}>
+export default function PostComponent({ item }) {
+  
+  if(item.url){
+    return (
+      <View
+        style={{
+          height: cardHeight,
+          width: cardWidth,
+          marginHorizontal: 20,
+          marginVertical: 3,
+          borderRadius: 20,
+          backgroundColor: "black",
+        }}
+      >
         
-        <Image source={require(item.image)}></Image>
-        
-    </View>
-  )
+        <Image
+          source={{uri: item.url}}
+          style={{
+            flex: 1,
+            width: undefined,
+            height: undefined,
+            borderRadius: 20,
+            resizeMode: "contain",
+          }}
+        />
+      </View> 
+    );
+  }
+  
 }
